8533acdc133b782ac09130cd30c647d01d230e35
venkates97 <venkates.official@gmail.com>
2024-03-27 19:11:47 +0530

    after push

---
+++ src/componets/All.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { dataset } from '../DataSet'
+function All() {
+    return (
+        <>
+        <div className="container">
+        <div className="blogCraetion">
+        {
+                dataset.map((e) => {
+                    return (
+                    <>
+                    <div class="card" style={{ width: "18rem" }}>
+                        <img src={e.img} class="card-img-top" alt={e.tittle} />
+                        <div class="card-body">
+                            <h5 class="card-title">{e.tittle}</h5>
+                            <p class="card-text text-muted">By: {e.author}</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                    </>
+                    )
+                })
+            }
+        </div>
+        </div>
+        </>
+    )
+}
+
+export default All
---
+++ src/componets/Cybersecurity.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { dataset } from '../DataSet';
+
+function Cybersecurity() {
+  return (
+    <>
+        <div className="container">
+        <div className="blogCraetion">
+        {
+                dataset.map((e) => {
+                 if (e.category ==="cyber_security") {
+                  return (
+                    <>
+                    <div class="card" style={{ width: "18rem" }}>
+                        <img src={e.img} class="card-img-top" alt={e.tittle} />
+                        <div class="card-body">
+                            <h5 class="card-title">{e.tittle}</h5>
+                            <p class="card-text text-muted">By: {e.author}</p>
+                            <a href="#" class="btn btn-primary">Read More</a>
+                        </div>
+                    </div>
+                    </>
+                    )
+                 }
+                    
+                })
+            }
+        </div>
+        </div>
+        </>
+  )
+}
+
+export default Cybersecurity
